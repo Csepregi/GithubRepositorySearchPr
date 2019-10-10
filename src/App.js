5897352c58022c38dbb855bdf82fcb9ebdf387c3
csepregi <csepregigabor02@gmail.com>
2019-10-10 14:56:29 +0200

    set up the folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import repositoriService from './services/repositories';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+  
+  const [ datas, setRepos] = useState([]);
 
-  const [repositories, setRepositories] = useState([]);
+  
 
   useEffect(() => {
-    repositoriService
-    .search()
-    .then(initialNotes => {
-      setRepositories(initialNotes)
-      console.log(initialNotes);
-    })
-}, [])
+    const fetchData = async () => {
+    const result = await axios(
+      `https://api.github.com/search/repositories?q=mapbox`
+    );
+    setRepos(result.data.items);
+    //setLanguages(result.data.languages);
+    console.log(result.data.items)
+    };
+    
+    fetchData()
+  }, []);
+
+  const rows = () => datas.map(note =>
+    <li key={note.id}>{note.full_name}</li>
+  )
 
   return (
-    <div className="App">
-     
+    <div>
+     {rows()}
     </div>
   );
 }
-
 export default App;
---
+++ src/components/RepoSearch.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RepoSearch() {
+  return (
+    <div>
+
+    </div>
+  )
+}
